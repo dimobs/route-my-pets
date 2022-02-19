6d2c86d648443b12a9354c6b9350137709496fee
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-02-19 08:32:09 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Header from "./components/Header";
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+import Create from './components/Create';
+import MyPets from './components/MyPets';
+import Edit from './components/Edit';
+import * as authService from './services/authService';
 
 function App() {
+  const [iserInfo, setUserInfo] = useState({isAuthenticated: false, username: ""})
+
+  useEffect(() => {
+    let user = authService.getUser();
+
+    setUserInfo({
+      isAuthenticated: Boolean(user),
+      user,
+    });
+  
+  }, []);
+
+  const onLogin = (username) => {
+    setUserInfo({
+      isAuthenticated:true, 
+      user: username,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+    
+    <Header />
+    
+        <main id="site-content">
+<Routes>
+<Route path='/' element={<Dashboard />} />
+<Route path='/Login' element={<Login onLogin={onLogin} />} />
+<Route path='/Register' element={<Register />} />
+<Route path='/My-Pets' element={<MyPets />} />
+<Route path='/Create' element={<Create />} />
+<Route path='/Edit' element={<Edit />} />
+</Routes>
+        </main>
+
+        <footer id="site-footer">
+            <p>@PetMyPet</p>
+        </footer>
+
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+    return(
+<header id="site-header">
+            <nav className="navbar">
+                <section className="navbar-dashboard">
+                    <Link to="/">Dashboard</Link>
+                    <div id="guest">
+                        <a className="button" href="/login">Login</a>
+                        <Link to='/register' className="button">Register</Link>
+                    </div>
+                    <div id="user">
+                        <span>Welcome, email</span>
+                        <Link className="button" to="my-pets">My Pets</Link>
+                        <Link className="button" to="/create">Add Pet</Link>
+                        <Link className="button" to="/logout">Logout</Link>
+                    </div>
+                </section>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
