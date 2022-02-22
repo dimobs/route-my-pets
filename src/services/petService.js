5998a6ba2c5da75af7beabdd7268d3a44c737c14
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-02-22 20:41:26 +0200

    Add pet

--- src/services/petService.js
+++ src/services/petService.js
@@ -7,3 +7,17 @@ let result = Object.values(pets)
     return result;
 
 }
+
+export const create = async (petData) => {
+let response = await fetch(`${baseUrl}/pets`, {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json',
+    },
+    body: JSON.stringify(petData)
+});
+
+let result = await response.json();
+
+return result;
+};
